396761d408a24ab178e414e08370deacb2bcac2e
isabella3412 <izzybella17@icloud.com>
2018-06-19 15:27:02 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,16 +2,13 @@
 
 # Try starting out with puts'ing a string.
 puts "Dear Hermione,"
-
-
+puts "                  "
 puts "You are cordially invited to the Best Halloween Party Ever on"
 puts "October 31 at 6pm"
 puts "Please RSVP no later than October 30."
 puts "                       "
-
 puts "Sincerely,"
-
-
+puts "                       "
 puts "Harry Potter"
 
 
